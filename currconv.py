84b1a5e3934e80a25426085e7fa887313daa9ec7
Guillaume Marcel <guillaume.k.marcel@gmail.com>
2021-04-30 18:49:27 +0200

    refactor converter

--- currconv.py
+++ currconv.py
@@ -2,93 +2,98 @@
 
 import argparse
 import re
+from dataclasses import dataclass
 
 import bs4
 import requests
 
 
-def _validate_currency(curr: str) -> str:
-    format_regex = re.compile(r"^[a-zA-Z]{3}$")
+XE_URL = 'https://www.xe.com/currencyconverter/convert?Amount={amount}&From={source}&To={target}'
 
-    if not format_regex.match(curr):
-        raise argparse.ArgumentError()
+@dataclass
+class CurrencyConverter:
+    amount: float
+    source: str
+    target: str
 
-    return curr
+    def convert(self) -> str:
+        return self._fetch_conversion() if self.source != self.target else str(self.amount)
+
+    def _fetch_conversion(self) -> str:
+        """
+        This method processes the command and returns the converted amount.
+
+        It calls online and parses the conversion result.
+
+        :return: The converted amount.
+        """
+        req = requests.get(XE_URL.format(amount=self.amount,source=self.source, target=self.target))
+        req.raise_for_status()
+
+        html_result = bs4.BeautifulSoup(req.text, 'html.parser').find(class_='iGrAod')
+        html_result.find(class_='faded-digits').decompose()
+
+        return re.sub("[^0-9.]", "", html_result.text)
+
+    @staticmethod
+    def validate(val: str, cache=[]) -> str:
+        """
+        This function validates the input for currency conversion.
+
+        This function is therefore called once for each of the positional parameters.
+        Three parameters are given for currency conversion, in the following order:
+        - the amount of money to convert.
+        - the currency code of the currency to convert.
+        - the currency code to convert to.
+
+        :return: The validated argument.
+        """
+        if len(cache) == 0:
+            if not re.match(r"^([0-9]*[.])?[0-9]+$", val):
+                raise argparse.ArgumentError()
+        else:
+            if not re.match(r"^[a-zA-Z]{3}$", val):
+                raise argparse.ArgumentError()
+        cache.append(val)
+
+        return val
 
 def get_parser() -> argparse.ArgumentParser:
     """
-    This function defines the parser arguments.
+    This function defines the command parser.
 
     :return: The parser.
     """
     parser = argparse.ArgumentParser(
         prog='currconv',
-        description='Currency Converter'
+        description='Currency Checker'
     )
 
     parser.add_argument(
-        '-s', '--source',
+        '-x',
         required=False,
-        default='USD',
-        metavar='S',
-        type=_validate_currency,
-        help='The currency to convert'
-    )
-
-    parser.add_argument(
-        '-t', '--target',
-        required=False,
-        default='USD',
-        metavar='T',
-        type=_validate_currency,
-        help='The currency needed'
-    )
-
-    parser.add_argument(
-        'amount',
-        metavar='A',
-        type=int,
-        help='The amount to convert'
+        metavar=('A', 'S', 'T'),
+        type=CurrencyConverter.validate,
+        nargs=3,
+        help='Converts the amount A from source currency S to target currency T'
     )
 
     return parser
 
-def _fetch_conversion(amount: float, source: str, target: str) -> float:
-    """
-    This function processes the command and returns the converted amount.
-
-    It calls online and parses the conversion result.
-
-    :return: The converted amount.
-    """
-    req = requests.get(f'https://www.xe.com/currencyconverter/convert?Amount={amount}&From={source}&To={target}')
-    req.raise_for_status()
-
-    html_result = bs4.BeautifulSoup(req.text, 'html.parser').find(class_='iGrAod')
-    html_result.find(class_='faded-digits').decompose()
-
-    return float(re.sub("[^0-9.]", "", html_result.text))
-
-def parse_cmd(parser: argparse.ArgumentParser) -> float:
+def parse_cmd(parser: argparse.ArgumentParser) -> str:
     """
     This function parses the command.
 
-    :return: The converted amount.
+    It calls the appropriate logic to successfully process and parse the input command.
+
+    :return: The command result.
     """
-    amount: float
+    result: str
 
     parsed_cmd = parser.parse_args()
-
-    if parsed_cmd.source != parsed_cmd.target:
-        amount = _fetch_conversion(parsed_cmd.amount, parsed_cmd.source, parsed_cmd.target)
-    else:
-        amount = float(parsed_cmd.amount)
-
-    return amount
+    if parsed_cmd.x:
+        return CurrencyConverter(*parsed_cmd.x).convert()
 
 
 if __name__ == '__main__':
-
-    parser = get_parser()
-    print(parse_cmd(parser))
-
+    print(parse_cmd(get_parser()))
